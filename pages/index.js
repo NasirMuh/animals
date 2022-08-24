c357c10a2d128346ee01b2468175b85e9d22df99
NasirMuh <octobernine1995@gmail.com>
2022-08-25 02:49:13 +0500

    adding imagr url

--- pages/index.js
+++ pages/index.js
@@ -28,14 +28,12 @@ export default function Home({ animals }) {
             animals?.map((animal) => {
               return <div key={animal._id} className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                 <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
+                  {/* <img src={builder.image(animal.image).width(200).url()} alt="image" className='rounded-full' /> */}
+                <Image src={builder.image(animal.image).url() || "Nasir.jpg"}  width={125} height={125} alt="Picture" className='rounded-full'/>
                 </div>
-                {/* <Image src={builder.image(animal.image).width(200).url() || "Nasir.jpg"} alt="Picture" /> */}
                 <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                  <h2 className="text-gray-900 text-lg title-font font-medium mb-2">{animal.title}</h2>
-                  <p className="leading-relaxed text-base">{animal.description}</p>
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-2">{animal.name}</h2>
+                  <p className="leading-relaxed text-base">{animal._type}</p>
                   <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -61,7 +59,7 @@ const client = createClient({
 });
 
 export async function getStaticProps() {
-  const animals = await client.fetch(`*[_type == "category"]`);
+  const animals = await client.fetch(`*[_type == "author"]`);
   return {
     props: {
       animals
